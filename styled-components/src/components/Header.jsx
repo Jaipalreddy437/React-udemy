d0679c9da1300844a0da0b87f52fcdb617d42961
Jaipal Ramidi <jaipal.ramidi@ONPASSIVE.ORG>
2023-12-13 11:28:27 +0530

    conditional clasess added

--- styled-components/src/components/Header.jsx
+++ styled-components/src/components/Header.jsx
@@ -2,8 +2,8 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
+    <header className='flex flex-col mb-16 items-center justify-center'>
+      <img src={logo} alt="A canvas" className='object-contain w-44 h-44 font-title' />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
